58ec66a7a9facc3209ae919ae230808eb6d3aaea
MEENAKSHI-D <meenakumara2025@gmail.com>
2026-06-08 18:40:42 +0530

    fixed mobile issues and contact overflow

--- script.js
+++ script.js
@@ -40,3 +40,36 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
+
+const words = ["Full Stack Developer", "Software Developer", "Designer"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+const typing = document.getElementById("typing");
+
+function type() {
+    currentWord = words[i];
+
+    if (isDeleting) {
+        typing.textContent = currentWord.substring(0, j--);
+    } else {
+        typing.textContent = currentWord.substring(0, j++);
+    }
+
+    if (!isDeleting && j === currentWord.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % words.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
